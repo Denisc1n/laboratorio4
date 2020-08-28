31b016ba7fc504ca5c8b50fa576c826e4208fcb1
denisc1n <denis.l.pedemonte@gmail.com>
2020-08-27 22:07:19 -0300

    Ejercicio Uno, Suma y Promedio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'applabo4';
+export class AppComponent implements OnInit {
+  public valorUno = 0;
+  public valorDos = 0;
+  public resultadoSuma = 0;
+  public resultadoPromedio = 0;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  calcularSumaOnClick() {
+    this.resultadoSuma = this.valorUno + this.valorDos;
+  }
+  calcularPromedioOnClick() {
+    this.resultadoPromedio = (this.valorUno + this.valorDos) / 2;
+  }
+  limpiarCuadrosOnClick() {
+    this.resultadoSuma = 0;
+    this.resultadoPromedio = 0;
+    this.valorDos = 0;
+    this.valorUno = 0;
+  }
 }
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  public ejemplo = 'Hola Mundo';
+  public ejemplo2 = 'Hola Mundo';
+  constructor() {}
+
+  ngOnInit() {}
+
+  PruebaClick() {
+    this.ejemplo2 = 'Ejecutamos el evento!!!';
+  }
+}
